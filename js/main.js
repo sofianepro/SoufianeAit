a76d72ebf422b3d5788ed91143214745afea7b09
sofianepro <thesofianexp@hotmail.com>
2017-07-28 17:51:10 +0100

    fix div to center and add scss syntex

--- js/main.js
+++ js/main.js
@@ -2,12 +2,16 @@ $(function () {
 	
 	// Change Header Height
 	$('.intro').height($(window).height());
+
 	$(window).resize(function() {$('.intro').height($(window).height());})
 
 	// Adjust intro item Center
 	$('.intro .welcome').css('marginTop', ($(window).height() - $('.welcome').height()) / 2.4 );
 
+	$('.intro').css('paddingTop', ($(window).height() - $('.mycontainer').height()) / 2 );
+
   	$(window).resize(function(){
+  		$('.intro').css('paddingTop', ($(window).height() - $('.mycontainer').height()) / 2 );
   	})
 
 
